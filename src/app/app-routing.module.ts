23db8390020273ea791cd73c5f07089e57715ea1
amirarania <81365125+amirarania@users.noreply.github.com>
2021-04-20 16:35:36 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ import { LoginComponent } from './login/login.component';
 import { AjouterChauffeurComponent } from './chauffeurs/ajouter-chauffeur/ajouter-chauffeur.component';
 import { ProfilComponent } from './profil/profil.component';
 import { ReservationComponent } from './reservation/reservation.component';
-import { MajComponent } from './profil/maj/maj.component';
 import { makeBindingParser } from '@angular/compiler';
-import { MdpComponent } from './profil/mdp/mdp.component';
+
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
-import { AuthGuard } from "./shared/guard/auth.guard";
+import { MdpComponent } from './profil/mdp/mdp.component';
+import { passwordComponent } from './password/password.component';
+import { ModifierComponent } from './modifier/modifier.component';
+import { DetailsComponent } from './details/details.component';
 
 
 
@@ -25,24 +27,24 @@ const routes: Routes = [
     path:'',
     component:LayoutsComponent,
     children:[
-      {path: '', component:homeComponent, canActivate:[AuthGuard]},
-      {path: 'Home', component:homeComponent},
-      {path: 'Chauffeurs', component:ChauffeursComponent},
-      {path: 'Passagers', component:PassagersComponent},
+      {path: 'home', component:homeComponent},
+      {path: 'chauffeurs', component:ChauffeursComponent},
+      {path: 'passagers', component:PassagersComponent},
       {path: 'ajouter-chauffeur', component:AjouterChauffeurComponent},
       {path:'profil',component:ProfilComponent},
       {path:'reservation',component:ReservationComponent},
-      {path:'maj',component:MajComponent},
       {path:'mdp',component:MdpComponent},
       { path: 'forgot-password', component: ForgotPasswordComponent },
-      { path: 'verify-email-address', component: VerifyEmailComponent }
-
+      { path: 'verify-email-address', component: VerifyEmailComponent },
+      {path: 'modifier' , component: ModifierComponent},
+      {path: 'details' , component:DetailsComponent}
   
     ]
   },
   {
     path:'login', component: LoginComponent
-  }
+  },
+  {path: 'password',component:passwordComponent},
 ];
 
 @NgModule({
