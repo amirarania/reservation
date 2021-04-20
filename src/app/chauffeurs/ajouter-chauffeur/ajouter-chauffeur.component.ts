577b551e930d22a809477a854a6a2bba7dbec41a
amirarania <81365125+amirarania@users.noreply.github.com>
2021-04-20 16:17:24 +0200

    Update ajouter-chauffeur.component.ts

--- src/app/chauffeurs/ajouter-chauffeur/ajouter-chauffeur.component.ts
+++ src/app/chauffeurs/ajouter-chauffeur/ajouter-chauffeur.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Compte } from 'src/app/classes/compte';
 import { Personne } from 'src/app/classes/personne';
@@ -12,12 +13,21 @@ import { chauffeursService } from 'src/app/services/chauffeurs.service';
 export class AjouterChauffeurComponent implements OnInit {
 
   personne: Personne = new Personne();
- compte: Compte = new Compte();
+  compte: Compte = new Compte();
   submitted = false;
+  chauffeurForm: FormGroup;
 
-  constructor(private PersonneService: chauffeursService,private router:Router) { }
+  constructor(private PersonneService: chauffeursService,private router:Router,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.chauffeurForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.minLength(3)]],
+      prenom: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      tel: ['',[Validators.required, Validators.minLength(8),Validators.maxLength(8)]],
+      civilite: ['', [Validators.required, Validators.minLength(1)]],
+      grade: ['chauffeur' ],
+    });
   }
 
   savePersonne(): void {
@@ -29,15 +39,20 @@ export class AjouterChauffeurComponent implements OnInit {
     record['tel']= this.personne.tel;
     record['civilite']= this.personne.civilite;
     record['grade']= 'chauffeur';
-    this.PersonneService.ajouterchauffeurs(record).then(() => {
+    console.log(record);
+    this.PersonneService.ajouterchauffeurs(this.chauffeurForm.value).then(() => {
       console.log('Created new item successfully!');
       this.submitted = true;
-      this.router.navigate(['/Chauffeurs']);
+      this.router.navigate(['/chauffeurs']);
     });
   }
 
   newPersonne(): void {
-    this.submitted = false;
+    this.submitted = true;
+    if (this.chauffeurForm.invalid) {
+      return;
+  }
     this.personne = new Personne();
   }
+
 }
