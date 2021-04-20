0b5017a1ac4d83ba7ccc2703aef9be420746c6d9
amirarania <81365125+amirarania@users.noreply.github.com>
2021-04-20 16:25:04 +0200

    Update navbar.component.ts

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  userinfo: any;
 
-  constructor() { }
+  constructor( public authService: AuthService) { }
+
 
   ngOnInit(): void {
+    this.userinfo =JSON.parse(localStorage.getItem('currentuser'));
+
   }
 
 }
